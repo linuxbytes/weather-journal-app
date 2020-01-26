33e78451630992e4dbdbce3fc3d41651e291f41e
linuxbytes <6693857+linuxbytes@users.noreply.github.com>
2020-01-25 19:07:30 -0500

    feat: add global and get function

--- server.js
+++ server.js
@@ -2,26 +2,33 @@
 projectData = {};
 
 // Require Express to run server and routes
-const express =  require("express");
+const express = require("express");
 // Start up an instance of app
 const app = new express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 
 const port = 3000;
 // Setup Server
 const server = app.listen(port, listening);
-function listening(){
-    console.log(server);
-    console.log(`running on localhost: ${port}`);
-};
+function listening() {
+  //console.log(server);
+  console.log(`running on localhost: ${port}`);
+}
 
+//make a GET request
+app.get("/all", sendData);
+
+function sendData(req, res) {
+  res.send(projectData);
+  console.log(projectData);
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,8 @@
+// Personal API Key for OpenWeatherMap API [0c0287eb61f14df9fc63debf39b1083e]
 /* Global Variables */
+const key = '&units=metric&APPID=0c0287eb61f14df9fc63debf39b1083e';
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
