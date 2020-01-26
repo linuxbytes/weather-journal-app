b5f7d30daf0cb0c07acd6870de5bbb0ade8fdac0
linuxbytes <6693857+linuxbytes@users.noreply.github.com>
2020-01-26 18:32:37 -0500

    feat: POST request to our route

--- website/app.js
+++ website/app.js
@@ -6,3 +6,25 @@ const baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+// use to make a POST request to our route
+const postData = async ( url = '', data = {})=>{
+    console.log(data);
+      const response = await fetch(url, {
+      method: 'POST', 
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+     // Body data type must match "Content-Type" header        
+      body: JSON.stringify(data), 
+    });
+
+      try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+      }catch(error) {
+      console.log("error", error);
+      }
+  }
